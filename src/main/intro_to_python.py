37b6dd0987405de2d7f900e16e07439712db6be0
s-hassanin <82418172+s-hassanin@users.noreply.github.com>
2023-01-22 14:13:36 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,8 +1,7 @@
+#This code is written by: Salma Hassanin
+
 import numpy as np
 
-# Print a specific 3x3 matrix where a cell is 1 if i == j, else 0
-# 2. Print the 3x3 matrix from #1 and then add 3 to every cell where i ≠j
-# 3. Print the 3x3 matrix from #2 as a 3x2 by deleting the last column from the matrix created
 
 
 array = np.zeros((3, 3), dtype=int)
